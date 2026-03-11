14c280e4733585bcbd197a7ad924f94146b7699c
Eric Hauser <ewhauser@gmail.com>
2026-03-11 16:39:34 -0600

    examples: add sqlite-backed filesystem demo (#15)

--- examples/go.mod
+++ examples/go.mod
@@ -4,9 +4,12 @@ go 1.25.0
 
 require (
 	github.com/ewhauser/jbgo v0.0.0
+	github.com/ncruces/go-sqlite3 v0.31.1
 	github.com/openai/openai-go v1.12.0
+	golang.org/x/term v0.40.0
 	google.golang.org/adk v0.6.0
 	google.golang.org/genai v1.49.0
+	mvdan.cc/sh/v3 v3.13.0
 )
 
 require (
@@ -45,7 +48,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mikefarah/yq/v4 v4.52.4 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
-	github.com/ncruces/go-sqlite3 v0.31.1 // indirect
 	github.com/ncruces/julianday v1.0.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/tetratelabs/wazero v1.11.0 // indirect
@@ -67,14 +69,12 @@ require (
 	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/term v0.40.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
 	golang.org/x/tools v0.41.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260128011058-8636f8732409 // indirect
 	google.golang.org/grpc v1.78.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473 // indirect
-	mvdan.cc/sh/v3 v3.13.0 // indirect
 	rsc.io/omap v1.2.0 // indirect
 	rsc.io/ordered v1.1.1 // indirect
 )
---
+++ examples/sqlite-backed-fs/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	jbruntime "github.com/ewhauser/jbgo/runtime"
+)
+
+const defaultWorkDir = "/home/agent"
+
+func main() {
+	exitCode, err := run(context.Background(), os.Stdin, os.Stdout, os.Stderr, os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(exitCode)
+}
+
+func run(ctx context.Context, stdin io.Reader, stdout, stderr io.Writer, args []string) (int, error) {
+	opts, err := parseCLIOptions(stderr, args)
+	if err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			return 0, nil
+		}
+		return 1, err
+	}
+
+	rt, err := jbruntime.New(&jbruntime.Config{
+		FileSystem: jbruntime.CustomFileSystem(
+			sqliteFSFactory{dbPath: opts.dbPath},
+			opts.workDir,
+		),
+	})
+	if err != nil {
+		return 1, fmt.Errorf("create runtime: %w", err)
+	}
+
+	if opts.repl || (opts.script == "" && stdinIsTTY(stdin)) {
+		return runInteractiveShell(ctx, rt, stdin, stdout, stderr, opts.workDir)
+	}
+
+	script := opts.script
+	if script == "" {
+		data, err := io.ReadAll(stdin)
+		if err != nil {
+			return 1, fmt.Errorf("read stdin: %w", err)
+		}
+		script = string(data)
+	}
+
+	result, err := rt.Run(ctx, &jbruntime.ExecutionRequest{
+		Name:   "sqlite-backed-fs",
+		Script: script,
+	})
+	if err != nil {
+		return 1, fmt.Errorf("run script: %w", err)
+	}
+
+	if stdout != nil {
+		if _, err := io.WriteString(stdout, result.Stdout); err != nil {
+			return 1, fmt.Errorf("write stdout: %w", err)
+		}
+	}
+	if stderr != nil {
+		if _, err := io.WriteString(stderr, result.Stderr); err != nil {
+			return 1, fmt.Errorf("write stderr: %w", err)
+		}
+	}
+
+	return result.ExitCode, nil
+}
+
+type cliOptions struct {
+	dbPath  string
+	workDir string
+	repl    bool
+	script  string
+}
+
+func parseCLIOptions(stderr io.Writer, args []string) (cliOptions, error) {
+	var opts cliOptions
+
+	fs := flag.NewFlagSet("sqlite-backed-fs", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+	fs.StringVar(&opts.dbPath, "db", "", "host SQLite database file used to persist the sandbox filesystem")
+	fs.StringVar(&opts.workDir, "workdir", defaultWorkDir, "sandbox working directory")
+	fs.BoolVar(&opts.repl, "i", false, "run an interactive shell session")
+	fs.BoolVar(&opts.repl, "repl", false, "run an interactive shell session")
+	fs.StringVar(&opts.script, "script", "", "shell script to run; when empty the example reads from stdin")
+
+	if err := fs.Parse(args); err != nil {
+		return cliOptions{}, err
+	}
+	if opts.dbPath == "" {
+		return cliOptions{}, errors.New("--db is required")
+	}
+	if opts.repl && opts.script != "" {
+		return cliOptions{}, errors.New("--repl and --script cannot be used together")
+	}
+
+	return opts, nil
+}
