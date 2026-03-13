3701e6fe3d109411b7783d2ed5bb868fb6810ea2
Eric Hauser <ewhauser@gmail.com>
2026-03-13 16:25:06 -0600

    Migrate yes to command spec parser

--- commands/yes.go
+++ commands/yes.go
@@ -22,20 +22,45 @@ func (c *Yes) Name() string {
 }
 
 func (c *Yes) Run(ctx context.Context, inv *Invocation) error {
-	operands, mode, err := parseYesArgs(inv)
-	if err != nil {
-		return err
+	return RunCommand(ctx, c, inv)
+}
+
+func (c *Yes) Spec() CommandSpec {
+	return CommandSpec{
+		Name: "yes",
+		HelpRenderer: func(w io.Writer, _ CommandSpec) error {
+			_, err := io.WriteString(w, yesHelpText)
+			return err
+		},
+		VersionRenderer: func(w io.Writer, _ CommandSpec) error {
+			_, err := io.WriteString(w, yesVersionText)
+			return err
+		},
+		Options: []OptionSpec{
+			{Name: "help", Short: 'h', Long: "help", Help: "Print help"},
+			{Name: "version", Short: 'V', Long: "version", Help: "Print version"},
+		},
+		Args: []ArgSpec{
+			{Name: "string", ValueName: "STRING", Repeatable: true, Default: []string{"y"}},
+		},
+		Parse: ParseConfig{
+			InferLongOptions:      true,
+			StopAtFirstPositional: true,
+		},
 	}
-	switch mode {
-	case "help":
-		_, _ = io.WriteString(inv.Stdout, yesHelpText)
-		return nil
-	case "version":
-		_, _ = io.WriteString(inv.Stdout, yesVersionText)
-		return nil
+}
+
+func (c *Yes) RunParsed(ctx context.Context, inv *Invocation, matches *ParsedCommand) error {
+	if matches.Has("help") {
+		spec := c.Spec()
+		return RenderCommandHelp(inv.Stdout, &spec)
+	}
+	if matches.Has("version") {
+		spec := c.Spec()
+		return RenderCommandVersion(inv.Stdout, &spec)
 	}
 
-	buffer := prepareYesBuffer(yesArgsIntoBuffer(operands))
+	buffer := prepareYesBuffer(yesArgsIntoBuffer(matches.Args("string")))
 
 	writer := bufio.NewWriterSize(inv.Stdout, yesBufferSize)
 	for {
@@ -79,49 +104,6 @@ func prepareYesBuffer(buffer []byte) []byte {
 	return buffer
 }
 
-func parseYesArgs(inv *Invocation) (operands []string, mode string, err error) {
-	args := append([]string(nil), inv.Args...)
-	operands = make([]string, 0, len(args))
-	parsingOptions := true
-	for len(args) > 0 {
-		arg := args[0]
-		args = args[1:]
-
-		if !parsingOptions {
-			operands = append(operands, arg)
-			continue
-		}
-
-		switch {
-		case arg == "--":
-			parsingOptions = false
-		case arg == "-h":
-			return nil, "help", nil
-		case arg == "-V":
-			return nil, "version", nil
-		case yesMatchesLongOption(arg, "help"):
-			return nil, "help", nil
-		case yesMatchesLongOption(arg, "version"):
-			return nil, "version", nil
-		case arg == "-" || !strings.HasPrefix(arg, "-"):
-			operands = append(operands, arg)
-			parsingOptions = false
-		case strings.HasPrefix(arg, "--"):
-			return nil, "", exitf(inv, 1, "yes: unrecognized option '%s'\nTry 'yes --help' for more information.", arg)
-		default:
-			return nil, "", exitf(inv, 1, "yes: invalid option -- '%s'\nTry 'yes --help' for more information.", strings.TrimPrefix(arg, "-"))
-		}
-	}
-	return operands, "", nil
-}
-
-func yesMatchesLongOption(arg, name string) bool {
-	if !strings.HasPrefix(arg, "--") || arg == "--" {
-		return false
-	}
-	return strings.HasPrefix(name, strings.TrimPrefix(arg, "--"))
-}
-
 func yesBrokenPipe(err error) bool {
 	return errors.Is(err, io.ErrClosedPipe) ||
 		errors.Is(err, syscall.EPIPE) ||
@@ -143,3 +125,5 @@ Options:
 const yesVersionText = "yes (uutils coreutils) 0.7.0\n"
 
 var _ Command = (*Yes)(nil)
+var _ SpecProvider = (*Yes)(nil)
+var _ ParsedRunner = (*Yes)(nil)
